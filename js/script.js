2088bec8deca94e909291803a42f657eb208c0b5
Shahar Levi <shaharLe88@gmail.com>
2020-12-22 04:13:37 +0200

    add beat rulls

--- js/script.js
+++ js/script.js
@@ -4,23 +4,32 @@
 var selectionButtons,myScore, BeatsRules;
 
    
-   function initGame() {
-       myScore = 0;
-       selectionButtons = document.querySelectorAll('[data-choice]');
-       BeatsRules = [ 
-       {   
-        name: 'paper',
-        beats: 'rock'
-       },
-       {
-        name: 'scissors',
-        beats: 'paper'
-       },
-       {
-        name: 'rock',
-        beats: 'scissors'
-       }
-      ]
-   }
+function initGame() {
+  myScore = 0;
+  selectionButtons = document.querySelectorAll('[data-choice]');
+  BeatsRules = [ 
+  {   
+    name: 'paper',
+    beats: 'rock'
+  },
+  {
+    name: 'scissors',
+    beats: 'paper'
+  },
+  {
+    name: 'rock',
+    beats: 'scissors'
+  }
+ ]
+} 
    
    initGame();
+
+
+   selectionButtons.forEach(element => {
+    element.addEventListener('click', event => {
+        var buttonChoices = element.dataset.choice;
+        var mySelection = BeatsRules.find(choice => choice.name === buttonChoices);
+        makeSelection(mySelection);
+    });
+});
